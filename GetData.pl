415dd164d60b52fce65320d7590bb5bb9a2f05d9
sbenas1 <sbenas1@umbc.edu>
2015-11-09 10:56:45 -0500

    Some changes to script and a file with all the data.  Still is ugly though.

--- GetData.pl
+++ GetData.pl
@@ -17,11 +17,19 @@ use strict;
 
 #Used in loops
 my $num_days = 0;
+
+#I don't think this is necessary
 my @months = ("Dummy 0th Month", "January", "February", "March", "April", "May", "June", 
 	   "July", "August", "Septemter", "October", "November", "December");
+my $year = 2015;
 
-#The base URL where we are getting our Data.  I don't actually know it yet.
-my $data_url = "http://www.GiveMeTheWebsitePleaseAlex.com/";
+#The base URL where we are getting our Data.
+#http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KMDBALTI52&day=8&month=11&year=2014&graphspan=day&format=1
+my $data_url1 = "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KMDBALTI52";
+my $data_url2 = "&graphspan=day&format=1";	#This is some random thing for the URL
+
+
+my %data_hash;
 
 #For data input/output
 my $storage_directory = "RawData/";
@@ -36,7 +44,12 @@ open(OUTFILE, '>', "$host_location/$write_file") or die "FATAL: Could not open $
 #Loop through each month,
 for(my $m = 1; $m <= 12; $m++){
 
+
+
 	my $current_month = $months[$m];
+
+
+
 
 	if ($m == 1 || $m == 3 || $m == 5 || $m == 7 || $m == 8 || $m == 10 || $m == 12){
 		$num_days = 31;
@@ -46,11 +59,25 @@ for(my $m = 1; $m <= 12; $m++){
 		$num_days = 30;
 	}
 
+	if ($m < 11){
+		$year = 2015;
+	} else {
+		$year = 2014;
+	}
+
+	print "Getting data for month of $current_month, $year.\n";
+	
+
 	#Loop through every day in current month
 	for (my $d = 1; $d <= $num_days; $d++){
 
+		#http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KMDBALTI52&day=8&month=11&year=2014&graphspan=day&format=1
+	
 		#Doesn't currently work because I don't know what the URL is supposed to be.  Waiting on Alex to get back tom
-		#system("wget -q $data_url -O $host_location/$storage_directory/$current_month" . "_" . "$d.data");
+
+		my $url = "'$data_url1" . "&day=$d" . "&month=$m" . "&year=$year" . "$data_url2'";		
+		#print "$url \n";
+		system("wget -q $url -O $host_location/$storage_directory/$m" . "_" . "$d.data");
 				
 	}
 
@@ -68,11 +95,20 @@ foreach my $f (@files){
 	while (my $l = <READ>){
 
 		#This should parse the line first so it is something like "Number Of Minutes since Midnight, Jan 01st:Temperature in Fahrenheight"
+		
+		next if (!($l =~ /^\d/));
+
+
+		my @temp_split = split(",", $l);
+		my @real_data = split(",", $temp_split[0]);
+		#my $temperature = $temp_split[1];
+		#my @date_and_time = split($real_data[0], " ");
+	
+		#print OUTFILE "$real_data[0] - $real_data[1]\n";
 		print OUTFILE $l;
-
 	}
-	close(READ) or warn "Warning: Could not close $f after reading.\n;
-
+	close(READ) or warn "Warning: Could not close $f after reading.\n";
+	unlink "$host_location/$storage_directory/$f" or warn "Warning: Could not remove file $f\n";
 }
 
 
